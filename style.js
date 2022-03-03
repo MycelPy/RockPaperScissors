589b539f8b8331b6c0a241331b72c2f098623b89
MycelPy <matus.michalcik@gmail.com>
2022-03-03 21:05:17 +0100

    Edited code for draw.

--- style.js
+++ style.js
@@ -1,17 +1,19 @@
 let player = 0,
-    computer = 0
+    computer = 0,
+    draw = 0
 
 const computerPlay = () => {
     const arr = ['rock', 'paper', 'scissors']
     return arr[Math.floor(Math.random() * arr.length)]
 }
 
-const playerPlay = prompt('Please enter Rock, Paper or Scissors!');
+const playerPlay = prompt('Please enter Rock, Paper or Scissors!').toLowerCase();
 
 const playRound = (computerPlay, playerPlay) => {
-    if (computerPlay.toLowerCase() === playerPlay.toLowerCase()) {
+    if (computerPlay === playerPlay) {
+        draw += 1
         return `Draw! Player: ${playerPlay} Computer: ${computerPlay}`
-    } else if (computerPlay.toLowerCase() === 'rock' && playerPlay.toLowerCase() === 'paper' || computerPlay.toLowerCase() === 'paper' && playerPlay.toLowerCase() === 'scissors' || computerPlay.toLowerCase() === 'scissors' && playerPlay.toLowerCase() === 'rock') {
+    } else if (computerPlay === 'rock' && playerPlay === 'paper' || computerPlay === 'paper' && playerPlay === 'scissors' || computerPlay === 'scissors' && playerPlay === 'rock') {
         player += 1
         return `Player wins round! Player: ${playerPlay} Computer: ${computerPlay}`
     } else {
@@ -32,6 +34,8 @@ const game = () => {
         console.log('Player wins!')
     } else if (player < computer) {
         console.log('Computer wins!')
+    } else {
+        console.log('After 5 rounds DRAW!')
     }
 }
 
