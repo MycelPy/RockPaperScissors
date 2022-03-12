b69c1b22927d0400c3b5db363b7d19800116c1fa
MycelPy <matus.michalcik@gmail.com>
2022-03-12 18:40:21 +0100

    Improved functionallity

--- style.js
+++ style.js
@@ -2,28 +2,32 @@ let player = 0,
     computer = 0,
     draw = 0
 
-
 const buttons = document.querySelectorAll('button')
+const playerko = document.querySelector('.player')
+const computerko = document.querySelector('.computer')
+const drawko = document.querySelector('.draw')
+const tab = document.querySelector('.tab')
 
-buttons.forEach(button => button.addEventListener('click', ))
-
-const playRound = () => {
+buttons.forEach(button => button.addEventListener('click', (e) => {
+    const playerPlay = e.target.classList[0]
 
     const computerPlay = () => {
         const arr = ['rock', 'paper', 'scissors']
         return arr[Math.floor(Math.random() * arr.length)]
     }
 
-    const playerPlay = prompt('Please enter Rock, Paper or Scissors!').toLowerCase();
-
-    if (computerPlay === playerPlay) {
+    if (computerPlay() === playerPlay) {
         draw += 1
-        return `Draw! Player: ${playerPlay} Computer: ${computerPlay}`
-    } else if (computerPlay === 'rock' && playerPlay === 'paper' || computerPlay === 'paper' && playerPlay === 'scissors' || computerPlay === 'scissors' && playerPlay === 'rock') {
+        drawko.textContent = `${draw}`
+        tab.textContent = `Draw! Player: ${playerPlay} Computer: ${computerPlay()}`
+    } else
+    if (computerPlay() === 'rock' && playerPlay === 'paper' || computerPlay() === 'paper' && playerPlay === 'scissors' || computerPlay() === 'scissors' && playerPlay === 'rock') {
         player += 1
-        return `Player wins round! Player: ${playerPlay} Computer: ${computerPlay}`
+        playerko.textContent = `${player}`
+        tab.textContent = `Player wins round! Player: ${playerPlay} Computer: ${computerPlay()}`
     } else {
         computer += 1
-        return `Computer wins round! Player: ${playerPlay} Computer: ${computerPlay}`
+        computerko.textContent = `${computer}`
+        tab.textContent = `Computer wins round! Player: ${playerPlay} Computer: ${computerPlay()}`
     }
-}
+}))
